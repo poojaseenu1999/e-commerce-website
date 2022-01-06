47c4ea72a8417a36f0ccce57e1251eafdfc4c591
pooja srinivasan <poojaseenu1999@gmail.com>
2022-01-06 11:19:33 -0600

    menu-item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import react from 'react';
+import React from 'react';
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 
 function App() {
