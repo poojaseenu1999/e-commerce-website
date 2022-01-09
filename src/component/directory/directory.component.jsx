3412a3aff5e84628ba64038a57734fbadbfaf9dc
pooja srinivasan <poojaseenu1999@gmail.com>
2022-01-09 15:40:34 -0600

    adding files

--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -12,21 +12,18 @@ class Directory extends React.Component {
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
           linkUrl: "shop/hats",
-         
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
           linkUrl: "shop/jackets",
-        
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
           linkUrl: "shop/sneakers",
-          
         },
         {
           title: "womens",
@@ -34,30 +31,32 @@ class Directory extends React.Component {
           size: "large",
           id: 4,
           linkUrl: "shop/womens",
-          
         },
         {
           title: "mens",
           imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
           size: "large",
           id: 5,
-          linkUrl: "shop/mens",
-         
+          linkUrl: "shop/mens" 
         },
       ],
     };
   }
 
-render(){
-    return(
-        <div className="directory-menu">
-          {this.state.sections.map(({title, imageUrl,id, size}) => {
-             return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}  />
-             
-          })}  
-        </div>
-    )
-}
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({id, ...otherSectionsProps}) => {
+          return (
+            <MenuItem
+              key={id}
+              {...otherSectionsProps}
+            />
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -5,7 +5,7 @@ import "./homepage.styles.scss";
 const HomePage = () => {
   return (
     <div className="homepage">
-    <Directory/>
+      <Directory />
     </div>
   );
 };
