2ce8c296e3d881b801c7b7e7fecd5617e6d977b9
pooja srinivasan <poojaseenu1999@gmail.com>
2022-02-01 14:47:26 -0600

    dropdown

--- src/component/collection-preview/collection-preview.component.jsx
+++ src/component/collection-preview/collection-preview.component.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import './collection-preview.styles.scss';
+import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 
-const CollectionPreview = ({ title, items}) => (
-<div className="collection-preview">
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
     <h1 className="title">{title.toLowerCase()}</h1>
-        <div className="preview">
-          {items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
-              <CollectionItem key ={id} {...otherItemProps}/>
-          ))}
-        </div>
-    
-</div>
-
-)
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
+);
 export default CollectionPreview;
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,17 +1,23 @@
 import { cartTypes } from "./cart.types";
 
 const INITIAL_STATE = {
-    hidden:true
+  hidden: true,
+  cartItems: [],
 };
 
-export const cartReducer = (state= INITIAL_STATE, action) => {
-    switch (action.type) {
-        case cartTypes.TOGGLE_CART_HIDDEN:
-          return {
-                ...state,
-                hidden: !state.hidden
-          }
-        default:
-            return state;
-    }
-}
+export const cartReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case cartTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    case cartTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload],
+      };
+    default:
+      return state;
+  }
+};
